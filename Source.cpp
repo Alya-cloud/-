4f3395f500e5e778d5585f637d34855892979761
Alya-cloud <114605247+Alya-cloud@users.noreply.github.com>
2024-02-03 12:42:03 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,49 +1,73 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
+#include <vector>
+#include <string>
+struct Contact {
+    std::string name;
+    std::string phoneNumber;
+};
 
-void fillArray(int* arr, int size) {            // Заполнение массива случайными числами от 1 до 50
-    for (int i = 0; i < size; ++i) {
-        arr[i] = rand() % 50 + 1;      // +1, чтобы было до 50 включительно
-    }
+typedef void (*ContactOperation)(std::vector<Contact>&);
+
+void addContact(std::vector<Contact>& contacts) {
+    Contact newContact;
+    std::cout << "Add a name: ";
+    std::cin >> newContact.name;
+    std::cout << "Add a phone number: ";
+    std::cin >> newContact.phoneNumber;
+
+    contacts.push_back(newContact);
+    std::cout << "Contact added" << std::endl;
 }
+void deleteContact(std::vector<Contact>& contacts) {
+    std::string nameToDelete;
+    std::cout << "Enter the name of a contact for delete: ";
+    std::cin >> nameToDelete;
 
-void printArray(int* arr, int size) {
-    for (int i = 0; i < size; ++i) {
-        std::cout << arr[i] << " ";
+    auto it = std::remove_if(contacts.begin(), contacts.end(),
+        [nameToDelete](const Contact& contact) {
+            return contact.name == nameToDelete;
+        });
+
+    if (it != contacts.end()) {
+        contacts.erase(it, contacts.end());
+        std::cout << "The contact has been deleted" << std::endl;
     }
-    std::cout << std::endl;
-}
-
-void findPairs(int* arr, int size, int targetSum) {             // Поиск и вывод пар элементов с заданной суммой
-    for (int i = 0; i < size - 1; ++i) {
-        for (int j = i + 1; j < size; ++j) {
-            if (arr[i] + arr[j] == targetSum) {
-                std::cout << "Pair found: " << arr[i] << " + " << arr[j] << " = " << targetSum << std::endl;
-            }
-        }
+    else {
+        std::cout << "The contact has not been found" << std::endl;
     }
 }
 
 int main() {
-    srand(time(nullptr));                        // Инициализирует генератор случайных чисел начальным значением
+    std::vector<Contact> phoneBook;            // Вектор для хранения контактов
 
-    int size;
-    std::cout << "Enter the size of the array: ";
-    std::cin >> size;
+    ContactOperation contactOperation;           // Указатель на функцию для операций с контактами
 
-    int* dynamicArray = new int[size];             // Выделяет память под динамический массив
-    fillArray(dynamicArray, size);
+    // Меню программы
+    while (true) {
+        std::cout << "choose the operation:" << std::endl;
+        std::cout << "1. Add the contact" << std::endl;
+        std::cout << "2. Delete the contact" << std::endl;
+        std::cout << "3. Exit" << std::endl;
 
-    std::cout << "Generated array: ";
-    printArray(dynamicArray, size);
+        int choice;
+        std::cin >> choice;
 
-    int targetSum;
-    std::cout << "Enter the target sum: ";
-    std::cin >> targetSum;
+        switch (choice) {
+        case 1:
+            contactOperation = addContact;
+            break;
+        case 2:
+            contactOperation = deleteContact;
+            break;
+        case 3:
+            return 0;
+        default:
+            std::cout << "Wrong choice. Try again" << std::endl;
+            continue;
+        }
 
-    findPairs(dynamicArray, size, targetSum);   // Передает указатель на динамический массив, кол-во элементов и введенную пользователем сумму
-    delete[] dynamicArray;     //  Освобождает память           
+        contactOperation(phoneBook);  // Вызов функции через указатель на функцию
+    }
 
     return 0;
 }
